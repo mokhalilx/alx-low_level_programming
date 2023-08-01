bc765f6528cef127ddbdbf30b62d120ee013956b
Mo Khalil <119762720+mokhalilx@users.noreply.github.com>
2023-08-01 12:47:11 +0300

    Update 7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,22 +1,61 @@
 #include "lists.h"
+/**
+  * listint_len - returns the number of elements ina linked listint_t list.
+  * @h: pointer to first element on list.
+  *
+  * Return: number of elements in list.
+  */
+size_t listint_len(const listint_t *h)
+{
+	int count;
+
+	count = 0;
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
 
 /**
- * get_nodeint_at_index - returns the node at a certain index in a linked list
- * @head: first node in the linked list
- * @index: index of the node to return
- *
- * Return: pointer to the node we're looking for, or NULL
- */
+  * get_nodeint_at_index - returns nth node of a listint_t linked list.
+  * @head: pointer to first elemnt of list.
+  * @index: index of node.
+  *
+  * Return: nth node,
+  * NULL if not found.
+  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    unsigned int i = 0;
-    listint_t *temp = head;
+	listint_t *current;
+	unsigned int count;
 
-    while (temp && i < index)
-    {
-        temp = temp->next;
-        i++;
-    }
+	current = head;
+	count = listint_len(head);
 
-    return (temp ? temp : NULL);
+	if (current == NULL)
+		count++;
+
+	if (count <= index)
+	{
+		return (NULL);
+	}
+	else
+	{
+		count = 0;
+		while (current != NULL)
+		{
+			if (count == index)
+			{
+				break;
+			}
+			else
+			{
+				count++;
+				current = current->next;
+			}
+		}
+		return (current);
+	}
 }
