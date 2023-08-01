067a46139a2450b60a42829821f34646d3df6c95
Mo Khalil <119762720+mokhalilx@users.noreply.github.com>
2023-08-01 12:42:09 +0300

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,39 +1,39 @@
 #include "lists.h"
-
 /**
- * delete_nodeint_at_index - deletes a node in a linked list at a certain index
- * @head: pointer to the first element in the list
- * @index: index of the node to delete
- *
- * Return: 1 (Success), or -1 (Fail)
- */
+  * delete_nodeint_at_index - deletes the node at index.
+  * @head: pointer to pointer of first elemnt of list.
+  * @index: index of node to be deleted.
+  *
+  * Return: 1 if successful, -1 if fails.
+  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    listint_t *temp = *head;
-    listint_t *current = NULL;
-    unsigned int i = 0;
+	unsigned int count;
+	listint_t *next_node, *temp;
 
-    if (*head == NULL)
-        return (-1);
+	temp = *head;
+	count = 0;
 
-    if (index == 0)
-    {
-        *head = (*head)->next;
-        free(temp);
-        return (1);
-    }
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		*head = temp->next;
+		free(temp);
+		return (1);
+	}
 
-    while (i < index - 1)
-    {
-        if (!temp || !(temp->next))
-            return (-1);
-        temp = temp->next;
-        i++;
-    }
+	while (temp != NULL && count < index - 1)
+	{
+		temp = temp->next;
+		count++;
+		if (temp == NULL || temp->next == NULL)
+			return (-1);
+	}
 
-    current = temp->next;
-    temp->next = current->next;
-    free(current);
+	next_node = temp->next->next;
+	free(temp->next);
+	temp->next = next_node;
 
-    return (1);
+	return (1);
 }
